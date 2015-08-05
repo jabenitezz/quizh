c6a5c96b19818d7618170dcb21f72824dd4a9f81
Jose A. Benítez Peláez <jabenitezz@gmail.com>
2015-08-05 09:24:03 +0200

    Ejercicio9 completo b

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -27,15 +27,20 @@ function buscarPreguntasComentarios(){
 
 
 function buscarPreguntas(){
+    console.log("Inicio buscarPreguntas : ");
     return models.Quiz.count();
 };
 
 
 function buscarComentarios(){
+    console.log("Inicio buscarComentarios : ");
     return models.Comment.count();
 };
 
-
+function papa(){
+    console.log("Inicio papa ");
+    return "";
+};
 
 
 var square = function () {
@@ -60,23 +65,24 @@ var estadisticas = {
     buscarPreguntas().then(function(count) {
       estadisticas.preguntas = count;
       console.log("Total de preguntas: " + count + " ");
-    }).then(buscarComentarios().then(function(count) {
+      return buscarComentarios();
+    }).then(function(count) {
       estadisticas.comentarios=count;
       console.log("Total de comentarios: " + count + " ");
-      })
-    ).then(function(){
-         estadisticas.comentariosporpregunta=estadisticas.comentarios/estadisticas.preguntas;
-         console.log("Total de comentariosporpregunta: " + estadisticas.comentariosporpregunta + " ");
-    }).then(buscarPreguntasNoComentarios().then(function(count) {
+      return;
+    }).then(function(){
+           estadisticas.comentariosporpregunta=estadisticas.comentarios/estadisticas.preguntas;
+           console.log("Total de comentariosporpregunta: " + estadisticas.comentariosporpregunta + " ");
+           return buscarPreguntasNoComentarios();
+    }).then(function(count) {
       estadisticas.preguntasnocomentarios=count;
-      console.log("Total de preguntas sin comentarios: " + count + " ")
-    })
-  ).then(
-    buscarPreguntasComentarios().then(function(count) {
-          estadisticas.preguntasconcomentarios=count;
-          console.log("Total de preguntas con comentarios: " + count + " ");
-        })
-  ).then(function() {
+      console.log("Total de preguntas sin comentarios: " + count + " ");
+      return;
+    }).then(function(){
+           estadisticas.preguntasconcomentarios=estadisticas.preguntas-estadisticas.preguntasnocomentarios;
+           console.log("Total de preguntas con comentarios: " + estadisticas.preguntasconcomentarios + " ");
+           return;
+    }).then(function() {
     console.log("valores: " + estadisticas.preguntas + ":" + estadisticas.comentarios + ":" + estadisticas.comentariosporpregunta + ":"
     + estadisticas.preguntasnocomentarios+ ":" + estadisticas.preguntasconcomentarios);
     res.render('quizes/statistics.ejs', {estadisticas: estadisticas, errors: []});
@@ -84,8 +90,13 @@ var estadisticas = {
   ).catch(function(error){next(error)})
 
   .finally(function () {
-    //next();
+    console.log("valores finally: " + estadisticas.preguntas + ":" + estadisticas.comentarios + ":" + estadisticas.comentariosporpregunta + ":"
+    + estadisticas.preguntasnocomentarios+ ":" + estadisticas.preguntasconcomentarios);
+    //res.render('quizes/statistics.ejs', {estadisticas: estadisticas, errors: []});
   });
+
+
+
 
 
 
